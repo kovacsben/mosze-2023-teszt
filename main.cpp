c6b9569447a480197766ed2f53ee3057b6288841
kovacsben <110032413+kovacsben@users.noreply.github.com>
2023-10-03 13:26:01 +0200

    Update main.cpp
    hibák kommentelése

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
+    int *b = new int[NELEMENTS]; //el van írva
+    std::cout << '1-100 ertekek duplazasa' // ; hiányzik , rossz aposztróf " ez kell
     for (int i = 0;)
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i; i++) // nem i ig megy a for ciuklus
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:" // ; hiányzik
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
     int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    for (int i = 0; i < N_ELEMENTS, i++) // , helyett ; kell
     {
-        atlag += b[i]
+        atlag += b[i] // ; hiányzik
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
